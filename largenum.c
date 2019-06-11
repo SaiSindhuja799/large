0c0cfedade2d7ed77bb1574224862cacf613e96e
SaiSindhuja799 <51689337+SaiSindhuja799@users.noreply.github.com>
2019-06-11 13:49:45 +0530

    Update largenum.c

--- largenum.c
+++ largenum.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,t;
+	int n,i,j,t,cnt=0;
 	scanf("%d",&n);
 	int a[n];
 	for(i=0;i<n;i++)
-	scanf("%d",&a[i]);
+	{
+		scanf("%d",&a[i]);
+		if(a[i]==0)
+		cnt++;
+	}
 	for(i=0;i<n-1;i++)
 	{
 		for(j=i+1;j<n;j++)
@@ -18,6 +22,12 @@ int main()
 			}
 		}
 	}
+	if(cnt==n)
+	printf("0");
+	else
+	{
 	for(i=n-1;i>=0;i--)
-	printf("%d",a[i]);
+	printf("%d ",a[i]);
+	}	
+		
 }
